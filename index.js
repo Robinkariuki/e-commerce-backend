ab48d76ea3a7e1bd47d97f2a8cf02320731189d4
Robinkariuki <robinkaris7@gmail.com>
2020-09-07 16:54:36 +0300

    installed packages

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ mongoose
 .connect('mongodb+srv://robin:hitman11@cluster0.dye5y.mongodb.net/products?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
   console.log('connected')
-  app.listen(5000)
+
 })
 .catch(error=>{
   console.log(error)
 });
+
+app.listen(5000)
---
+++ routes/user-routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/signup',usersControllers.signup)
+router.post('/login',usersControllers.login)
+
+
+module.exports = router;
