5b997576970bf0500e8afef08ca77b9b4d7fc12a
Robinkariuki <robinkaris7@gmail.com>
2020-09-10 16:13:54 +0300

    added multer middleware

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
+const fileUpload = require('../util/up-load')
+
 
 const productControllers = require('../controllers/products-controller');
 const router = express.Router()
 
 
 router.get('/products',productControllers.getProducts)
+
+
+router.post('/addproduct',
+fileUpload.single('image'),
+productControllers.createProduct)
+
+
 
 module.exports= router
