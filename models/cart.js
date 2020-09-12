04e8adb9053816c84cbbf18332ce5c0634e19bb2
Robinkariuki <robinkaris7@gmail.com>
2020-09-12 11:33:43 +0300

    added cart model

---
+++ models/cart.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+productId:{type:mongoose.Schema.Types.ObjectId, ref:'Product'},
+Quanity:{type:Number, required:true, min:[1,'Quantity can not be less then 1.']},
+price:{type:Number, required:true},
+total:{type:Number, required:true},
+timestamps:true
+})
+
+
+
+const cartSchema = new Schema({
+ items:[itemSchema],
+ subTotal:{default:0,type:Number},
+ timestamps:true
+})
+
+
+module.exports =mongoose.model('Cart',cartSchema)
