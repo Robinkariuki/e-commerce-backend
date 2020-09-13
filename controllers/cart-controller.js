1c685717da4f380169e2a996449b5cfd8461a9ee
Robinkariuki <robinkaris7@gmail.com>
2020-09-13 11:48:38 +0300

    added logic for adding item to cart

---
+++ controllers/cart-controller.js
@@ -0,0 +1,95 @@
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+const cart = require('../models/cart');
+
+// get all cart items method
+const  getAllCartItems = async ()=>{
+    const carts = await Cart.find().populate({
+        path: "items.productId",
+        select: "name price total"
+    });
+    return carts[0];
+}
+//add item to cart model method
+const addItem = async payload =>{
+    const newItem = await Cart.create(payload);
+    return newItem
+}
+
+
+
+
+const addItemToCart = async (req,res)=>{
+    const {productId} = req.body
+    const quantity = Number.parseInt(req.body.quantity);
+    try{
+        const cart = await Cart.getAllCartItems()
+        const productDetails = await Product.findById(productId);
+        if(!productDetails){
+            return res.status(500).json({ type:"Not found", msg:"invalid request"})
+        }
+        
+        //if cart Exists ---
+
+    if (cart){
+               //---- check if index exists ----
+      const indexFound = cart.items.findIndex(item => item.productId.id == productId);
+        //------this removes an item from the the cart if the quantity is set to zero,We can use this method to remove an item from the list  -------
+        if (indexFound !== -1 && quantity <= 0 ){
+            cart.items.splice(indexFound,1);
+            if(cart.items.length==0){
+            cart.subTotal = 0;
+        }else{
+            cart.subTotal = cart.items.map(item=>item.total).reduce((acc,next)=>acc+next);
+        }
+        
+        }
+       //----------check if product exist,just add the previous quantity with the new quantity and update the total price-------
+       else if (indexFound !== -1){
+        cart.items[indexFound].quantity = cart.items[indexFound].quantity + quantity;
+        cart.items[indexFound].total = cart.items[indexFound].quantity * productDetails.price;
+        cart.items[indexFound].price = productDetails.price
+        cart.subTotal = cart.items.map(item => item.total).reduce((acc, next) => acc + next); 
+       }
+       //----Check if Quantity is Greater than 0 then add item to items Array ----
+       else if (quantity > 0) {
+           cart.items.push({
+               productId:productId,
+               quantity:quantity,
+               price:productDetails.price,
+               total: parseInt(productDetails.price * quantity)
+           })
+           cart.subTotal = cart.items.map(item => item.total).reduce((acc, next) => acc + next);
+       }
+        //----if quantity of price is 0 throw the error -------
+        else{
+            return res.status(400).json({type: "Invalid", msg: "Invalid request"})
+        }
+        let data = await cart.save();
+        res.status(200).json({ type:"success", msg:"process Successful",data:data})
+    }
+//------------ if there is no user with a cart...it creates a new cart and then adds the item to the cart that has been created------------
+else {
+     const cartData = {
+         items :[{
+             productId: productId,
+             quantity:quantity,
+             total:parseInt(productDetails.price * quantity),
+             price: productDetails.price
+         }],
+         subTotal :parseInt(productDetails.prcie *quantity)
+
+    }
+    cart = await Cart.addItem(cartData)
+    // let data = await cart.save();
+    res.json(cart);
+}
+
+}catch (err){
+    console.log(err)
+    res.status(400).json({type:"invalid",msg:"Something went wrong", err:err})
+}
+
+
+
+}
