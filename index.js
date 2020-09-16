627a589cc76dbfd2f123a3fc832b6b753a9d9ab3
Robinkariuki <robinkaris7@gmail.com>
2020-09-16 14:26:10 +0300

    added cors headers

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ const usersRoutes = require("./routes/user-routes")
 const productsRoutes = require("./routes/product-routes")
 const cartRoutes = require("./routes/cart-routes");
 const path = require('path')
-
+var cors = require('cors')
 const app = express()
 
 
 app.use(bodyParser.json());
 app.use('/uploads/images',express.static(path.join('uploads','images')));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
+  next();
+});
 app.get("/", (req, res) => {
    res.json({ message: "API Working" });
    
