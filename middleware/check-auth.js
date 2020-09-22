f6a1ea70e9cfaa4893f699a9bd69008406313a09
Robinkariuki <robinkaris7@gmail.com>
2020-09-22 11:45:33 +0300

    authentication middleware

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,7 +1,19 @@
+const jwt = require('jsonwebtoken');
+
 const HttpError = require('../util/http-error')
 module.exports = (req,res,next)=>{
-    const token = req.headers.authorization.split(' ')[1]; //Authorization:'Bearer Token'
-    if(!token){
-        return 
-    }
-}
+ 
+        try {
+            const token = req.headers.authorization.split(' ')[1]; //Authorization:'Bearer Token'
+            if(!token){
+                throw new Error('Authentication failed')
+            }
+           const decodedToken= jwt.verify(token,'escapades')
+           req.userData ={userId:decodedToken.payload.user.id}
+           next();
+        } catch (err) {
+            const error = new HttpError('Authentication failed', 401)
+        return next(error)
+        }
+        
+    };
