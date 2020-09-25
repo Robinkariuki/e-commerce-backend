6a21093216d65e78e6ed28b1fbfbaea4c7368a0e
Robinkariuki <robinkaris7@gmail.com>
2020-09-25 11:52:58 +0300

    made changes

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,26 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const HttpError = require('../util/http-error')
-// (req, res, next) => {
-//     try {
-//       const token = req.header("x-auth-token");
-//       if (!token)
-//         return res
-//           .status(401)
-//           .json({ msg: "No authentication token, authorization denied." });
-  
-//       const verified = jwt.verify(token, process.env.JWT_SECRET);
-//       if (!verified)
-//         return res
-//           .status(401)
-//           .json({ msg: "Token verification failed, authorization denied." });
-  
-//       req.user = verified.id;
-//       next();
-//     } catch (err) {
-//       res.status(500).json({ error: err.message });
-//     }
-//   };
+
 module.exports = (req,res,next)=>{
  
         try {
@@ -29,9 +10,9 @@ module.exports = (req,res,next)=>{
                 throw new Error('Authentication failed')
             }
            const decodedToken= jwt.verify(token,'escapades') 
-           console.log(decodedToken)
+         
            req.userData = decodedToken
-           console.log(req.userData)
+          
            next();
         } catch (err) {
             const error = new HttpError('Authentication failed', 401)
