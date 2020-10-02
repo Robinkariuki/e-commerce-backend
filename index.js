b02a3b098b6d1ad6c191acbee90b34d5dfa1f960
Robinkariuki <robinkaris7@gmail.com>
2020-10-02 11:52:42 +0300

    deployment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cartRoutes = require("./routes/cart-routes");
 const path = require('path')
 const app = express()
 
-
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use('/uploads/images',express.static(path.join('uploads','images')));
@@ -44,4 +44,4 @@ mongoose
   console.log(error)
 });
 
-app.listen(5000)
+app.listen(PORT)
